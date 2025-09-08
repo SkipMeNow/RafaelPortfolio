2651893e0b039d458cc67bec967bc66af21f5f04
SkipMeNow <rambriz217@gmail.com>
2025-09-07 20:56:27 -0500

    Project fix

--- src/pages/project/Project.tsx
+++ src/pages/project/Project.tsx
@@ -2,11 +2,9 @@ import "./project.css";
 
 export const Project = () => {
   return (
-    <div className="home">
-      <div className="home__header">
-        <h1 className="home__title">Hi, I'm Rafael</h1>
-        <p className="home__location-text">Long Prairie, MN</p>
-        <p className="home__location-text">I'm a software engineer</p>"
+    <div>
+      <div>
+        <h1>Hi, I'm Rafael</h1>
       </div>
     </div>
   );
